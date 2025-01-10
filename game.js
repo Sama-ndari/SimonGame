9f2d87331f305e2aac3751377ef1ef2929221027
Samandari <Junior_julescesar10@hotmail.com>
2025-01-10 17:00:28 +0200

    last update
    i .........

--- game.js
+++ game.js
@@ -6,24 +6,42 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var userClickedPattern = [];
 
+
+
+// Check screen width and run code if screen is at least 500px
+function checkScreenWidth(mediaQuery) {
+    if (mediaQuery.matches) {
+        // Run your code here for mobile or tablet devices
+        console.log("Screen width is at least 1000px. Running code for mobile or tablet.");
+        
+        $(document).ready(function() {
+            $("#startInput").focus(function() {
+                if (!gameStarted) {
+                    nextSequence();
+                    gameStarted = true;
+                }
+            });
+            // Trigger the focus on the hidden input field when the page is loaded
+            $("#startInput").focus();
+        });
+    }
+}
+
+// Check screen width on page load
+let mediaQuery = window.matchMedia("(max-width: 1000px)");
+checkScreenWidth(mediaQuery);
+
+// Check screen width on resize
+window.addEventListener("resize", function() {
+    checkScreenWidth(mediaQuery);
+});
+
+
 $("body").keypress(function(event) {
     if(!gameStarted){//If the game has not started 
         nextSequence();
         gameStarted = true;
     }
-});
-
-
-$(document).ready(function() {
-    $("#startInput").focus(function() {
-        if (!gameStarted) {
-            nextSequence();
-            gameStarted = true;
-        }
-    });
-
-    // Trigger the focus on the hidden input field when the page is loaded
-    $("#startInput").focus();
 });
 
 function playSound(song) {
@@ -45,7 +63,13 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random()*4);//0 to 3
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+    if (level === 1) {
+        $("#"+randomChosenColour).fadeOut(500).fadeIn(100);
+    } else {
+        $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+    }
+    
+    //This sequence of fading out and fading in creates a visual effect where the selected button briefly disappears and reappears
     playSound(randomChosenColour);
 }
 
@@ -60,6 +84,7 @@ $("body").click(function(event) {
 function checkAnswer(currentLevel) {
     if(userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
         if (userClickedPattern.length === gamePattern.length){
+            //if the user has completed the entire sequence of colors up to the current level.
             setTimeout(function () {
                 nextSequence();
             }, 1000);
@@ -71,7 +96,7 @@ function checkAnswer(currentLevel) {
 }
 
 function gameOver() {
-    $("#level-title").text("Game Over, Press Any Key to Restart");
+    $("#level-title").text("Game Over, Press Any Key to Restart or Refrech The page");
     playSound("wrong");
     $("body").addClass("game-over");
     setTimeout(function() {
